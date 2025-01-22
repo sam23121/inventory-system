dfbf46ad294cb707dfe7aa2101d114c3f5ec88ad
sam23121 <smlalene@gmail.com>
2025-01-22 17:11:13 +0300

    first commit

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,12 +11,11 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     
-    POSTGRES_USER: str = os.getenv('POSTGRES_USER', 'avnadmin')
-    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD', 'AVNS_6MI-1EylUyneDiDaS2o')
-    POSTGRES_SERVER: str = os.getenv('POSTGRES_SERVER', 'pg-2924c7dc-smlalene-69f7.k.aivencloud.com')
-    POSTGRES_DB: str = os.getenv('POSTGRES_DB', 'defaultdb')
-    POSTGRES_PORT: int = int(os.getenv('POSTGRES_PORT', 20881))
-    #  DATABASE_URL: Optional[str] = 'postgresql://avnadmin:AVNS_6MI-1EylUyneDiDaS2o@pg-2924c7dc-smlalene-69f7.k.aivencloud.com:20881/defaultdb?sslmode=require'
+    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_SERVER: str = os.getenv('POSTGRES_SERVER')
+    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
+    POSTGRES_PORT: int = int(os.getenv('POSTGRES_PORT'))
     
     
     @property
